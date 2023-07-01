bb62100fd1bd6c07e48e9c236cda4f7190e1321b
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-07-01 13:16:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let inp = document.getElementById("evaluatedText");
+let ans = document.getElementById("letterCount");
+
+inp.addEventListener("keyup",()=> {
+		ans.innerText = inp.value.length;
+});
